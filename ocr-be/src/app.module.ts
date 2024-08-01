fd34c86194e9150d36ec0605817e4162b65f74d4
ia rigby <marvinzem@gmail.com>
2024-08-01 20:38:27 +0200

    implement ocr controller and service

--- ocr-be/src/app.module.ts
+++ ocr-be/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VersionController } from './version/version.controller';
+import { OcrController } from './ocr/ocr.controller';
+import { OcrService } from './ocr/ocr.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, VersionController],
-  providers: [AppService],
+  controllers: [AppController, VersionController, OcrController],
+  providers: [AppService, OcrService],
 })
 export class AppModule {}
