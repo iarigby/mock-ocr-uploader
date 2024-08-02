9d54db53d4dbaec7be60f51273300c6dba68d727
ia rigby <marvinzem@gmail.com>
2024-08-02 09:47:05 +0200

    handle connection error on the main page, add button to go to ocr

--- ocr-fe/src/app/page.tsx
+++ ocr-fe/src/app/page.tsx
@@ -3,6 +3,9 @@
 import {LoadingSpinner} from "@/components/ui/loadSpinner";
 import {getVersion} from "@/app/server";
 import {useEffect, useState} from "react";
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
+import {Alert} from "@/components/ui/alert";
 
 export default function Home() {
 
@@ -15,12 +18,20 @@ export default function Home() {
 
 function Version() {
     const [version, setVersion] = useState<string>();
+    const [error, setError] = useState<Error>();
     useEffect(() => {
-        getVersion()
+        setTimeout(() => getVersion()
             .then(setVersion)
+            .catch(setError), 3000)
+
     }, []);
+    if (error)
+        return <Alert variant='destructive'>{error.message}</Alert>
     if (version) {
-        return <h2>Current Version: {version}</h2>
+        return <div>
+            <h2>Current Version: {version}</h2>
+            <Link href={'/ocr'}><Button>Start Scanning</Button></Link>
+        </div>
     }
     return <div><p>Connecting to server, please wait </p><LoadingSpinner/></div>
 }
